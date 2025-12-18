5279c6d72c6f3cb7c52610f269661d193b34d404
xyloflare <pro.dev.mohit@gmail.com>
2025-12-18 21:26:55 +0530

    base v1 done ig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://xyloflare.github.io",
+  devToolbar: {
+    enabled: false,
+  },
+  server: { port: 4321, host: true },
+  vite: {
+    server: {
+      allowedHosts: "ohio-example.gl.at.ply.gg",
+    },
+    plugins: [tailwindcss()],
+  },
+  integrations: [react()],
+});
